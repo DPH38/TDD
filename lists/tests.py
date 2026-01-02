f35ab71c3554592091321267457f38c67dba9818
Diego Porto <halfporto@gmail.com>
2026-01-01 23:04:33 -0300

    Functional test now checks we can input a to-do item

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,4 @@
 from django.test import TestCase
-from django.http import HttpRequest
-from lists.views import home_page
-
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
