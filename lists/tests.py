3c4802d9759710996c1012ca484744e55740140d
Diego Porto <halfporto@gmail.com>
2026-01-08 22:45:23 -0300

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -11,10 +11,28 @@ class HomePageTest(TestCase):
         self.assertContains(response, '<form method="POST">')
         self.assertContains(response, 'name="item_text"')
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='Itemey 1')
+        Item.objects.create(text='Itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertContains(response, 'Itemey 1')
+        self.assertContains(response, 'Itemey 2')
+
     def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertContains(response, 'A new list item')
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertRedirects(response, '/')
+        
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
